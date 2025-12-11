f48eb66671fc711b2df2374dcf125c50b07b7566
SyntaxNate <new-shutter.nbt@gmail.com>
2025-12-11 00:32:51 -0500

    Projects Page first look

---
+++ portfolio/src/pages/Projects.jsx
@@ -0,0 +1,15 @@
+import { Box, Typography } from "@mui/material";
+
+function Projects() {
+
+    return (
+        <Box sx={{ p: 4 }}>
+            <Typography variant="h4"> Projects </Typography>
+            <Typography sx={{ mt: 2 }}>
+                This Page showcases and list your various apps.
+            </Typography>
+        </Box>
+    )
+}
+
+export default Projects;
